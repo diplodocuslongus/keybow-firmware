50b94d4639528b0fa28224045abd33c1ba29e68f
diplodocuslongus <hinoki.kuaimu@gmail.com>
2022-09-27 16:12:49 +0800

    new macro

--- sdcard/keys.lua
+++ sdcard/keys.lua
@@ -1,15 +1,13 @@
 require "keybow"
+require "layouts/ludomacro" -- Numberpad
 -- require "layouts/default" -- Numberpad
-require "layouts/sl_hyperkey" -- My Custom Numberpad
 
 -- Custom layouts (uncomment to enable) --
 
--- require "layouts/mouse" -- Mouse keys
 -- require "layouts/boilerplate" -- Handy bits of boilerplate text like Lorem Ipsum
 -- require "layouts/lightroom" -- Handy hotkeys for Adobe Lightroom Classic CC
 -- require "layouts/pico8" -- Controls for Pico-8
--- require "layouts/media_keys" -- Basic media controls (play/pause, volume etc)
--- require "layouts/extra_f_keys" -- keys from F13 to F24
+-- require "layouts/mediakeys" -- Basic media controls (play/pause, volume etc)
 -- require "layouts/midi_keys" -- Experimental MIDI layout
 -- require "layouts/blink" -- Numberpad with light feedback
 -- require "layouts/mini" -- Keybow MINI example layout
